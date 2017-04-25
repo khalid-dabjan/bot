845bf7c0a22c4a4de10003355b035f84764080d8
Khalid Dabjan <khalid.dabjan@gmail.com>
2017-04-25 17:46:36 +0000

    set up packages and snippets

---
+++ src/Commands/BotCommand.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace app;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class BotCommand extends Command
+{
+    /**
+     * Set the configuration for the command
+     */
+    protected function configure()
+    {
+        $this->setName('bot:move')
+            ->setDescription('Move the bot')
+            ->setHelp('Use this command to move the bot in a given fashion')
+            ->addArgument('direction', InputArgument::REQUIRED, 'The direction string for the bot to move');
+    }
+
+    /**
+     * Execute the command
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln($input->getArguments('direction'));
+    }
+}
